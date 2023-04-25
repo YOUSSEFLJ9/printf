e150490faa0b323f20510532bcd32ab877fc42d4
bouchra-e <essadanybouchra71@gmail.com>
2023-04-25 19:56:13 +0000

    update

---
+++ _print_switch.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* _print_switch - Select and print the corresponding format specifier
+* @format: A string containing zero or more format specifiers
+* @args: The va_list of arguments passed to _printf()
+* Return: The total number of characters printed, excluding the null byte
+*/
+int _print_switch(const char *format, va_list args)
+{
+int count = 0;
+
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c';
+count += _putchar(va_arg(args, int));
+break;
+case 's';
+count += _printstr(va_arg(args, char *));
+break;
+case 'd':
+case 'i';
+count += _printint(va_arg(args, int));
+break;
+case '%';
+count += _putchar('%');
+break;
+case '\0';
+return (-1);
+default:
+count += _putchar('%');
+count += _putchar(*format);
+}
+}
+else
+count += _putchar(*format);
+format++;
+}
+return (count);
+}
--- _printf.c
+++ _printf.c
@@ -8,51 +8,12 @@
   */
 int _printf(const char *format, ...)
 {
-	va_list argms;
-	int counter = 0;
+	va_list args;
+	int count = 0;
 	char c;
-	char *str;
+	char str;
 
-	va_start(argms, format);
-	while ((c = *format++) != '\0')
-	{
-		if (c == '%')
-		{
-			switch (*format++)
-			{
-			case 'c':
-				_putchar(va_arg(argms, int));
-				counter++;
-				break;
-			case '%':
-				_putchar('%');
-				counter++;
-				break;
-			case 's':
-				str = va_arg(argms, char *);
-				if (str == NULL)
-				{
-					_printstr("(null)");
-					counter += 6;
-				}
-				else
-					counter += _printstr(str);
-				break;
-			case 'd':
-			case 'i':
-			  counter += _printint(argms);
-			  break;
-			default:
-			  _putchar(*format);
-			  counter++;
-			  break;
-			}
-		}
-		else
-		{
-			_putchar(c);
-			counter++;
-		}
-	}
-	va_end(argms);
-	return (counter);
+	count = _print_switch(format, args);
+	va_end(args);
+	return (count);
+}
