27883021966f26d52d6b27ceaaff644c90825e3f
bouchra-e <essadanybouchra71@gmail.com>
2023-04-25 12:34:12 +0000

    updated

--- _printf.c
+++ _printf.c
@@ -38,6 +38,14 @@ int _printf(const char *format, ...)
 				else
 					counter += _printstr(str);
 				break;
+			case 'd':
+			case 'i':
+			  counter += _printint(argms);
+			  break;
+			default:
+			  _putchar(*format);
+			  counter++;
+			  break;
 			}
 		}
 		else
@@ -48,4 +56,30 @@ int _printf(const char *format, ...)
 	}
 	va_end(argms);
 	return (counter);
+}
+/**
+ * _printint - prints an integer
+ * @n: the integer to print
+ * Return: the number of characters printed.
+ */
+int _printint(int n)
+{
+  int i, sign, counter = 0;
+  char digits[10];
+  sign = (n < 0) ? -1 : 1;
+  i = 0;
+  do
+    {
+    digits[i++] = (char)((n % 10) * sign + '0');
+    n /= 10;
+    }
+  while (n != 0);
+  if (sign < 0)
+    digits[i++] = '-';
+  while (i-- > 0)
+    {
+      _putchar(digits[i]);
+      counter++;
+    }
+  return (counter);
 }
